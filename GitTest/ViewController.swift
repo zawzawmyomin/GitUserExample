95ee338377f039d49f43f0d52388c0fa6872d365
Zaw Myo Min <zawmyomin@nexlabs.co>
2017-07-07 11:00:03 +0630

    print message Add table view

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -8,7 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBAction func buttonClicked(_sender: UIButton){
+        print("This is  Git tutorial")
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 12
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
